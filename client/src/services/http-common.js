19dc6566e98161ff21f8f91a3cc1d2f6a06ed16a
johnnyarza <johnnyarza@gmail.com>
2020-07-15 18:44:04 -0300

    Changed baseUrl from backend

--- client/src/services/http-common.js
+++ client/src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://192.168.1.109:3001',
+  baseURL: 'http://localhost:3001',
   headers: {
     'Content-type': 'application/json',
   },
